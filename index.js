9e960365e7aa4ac7f107cf0969f42d3a609f8945
GHvladi <123510979+GHvladi@users.noreply.github.com>
2025-03-05 01:22:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+let count = 0
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-let count = 0
 
 function increment() {
     count += 1
@@ -13,3 +13,4 @@ function save() {
     countEl.textContent = 0
     count = 0
 }
+
